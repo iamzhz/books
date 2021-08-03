ad2f1a843464f9eb1c43ab152cca6da3c7a326ba
iamzhz <88195244+iamzhz@users.noreply.github.com>
2021-08-03 21:24:12 +0800

    Add files via upload

---
+++ books/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8" />
+</head>
+<body>
+    <title>My books</title>
+    <input type="button" value="查找图书" onclick="window.location.href='lookUp.php';" />
+    <br />
+    <input type="button" value="录入图书" onclick="window.location.href='enroll.php';" />
+    <br />
+    <input type="button" value="借书" onclick="window.location.href='borrow.php';" />
+    <br />
+    <input type="button" value="还书" onclick="window.location.href='return.php';" />
+    <br />
+    <input type="button" value="书库" onclick="window.location.href='lib.php';" />
+</body>
+</html>
